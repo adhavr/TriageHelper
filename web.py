f11a22f2b6bfd05a334f2d8d945992eeb1ad6226
adhavr <88722247+adhavr@users.noreply.github.com>
2025-02-23 21:40:20 -0500

    Update web.py

--- web.py
+++ web.py
@@ -115,5 +115,14 @@ def main():
             unsafe_allow_html=True,
         )
 
+        st.markdown(
+            """
+            #GithubIcon {
+              visibility: hidden;
+            }
+            """,
+            unsafe_allow_html=True,
+        )
+
 if __name__ == "__main__":
     main()
